8d96ff226e9a28e8345818b23039957c2d85077f
Pascal Heidmann <pascal@heidmann.io>
2023-05-16 01:06:35 +0200

    re-fetch album images every 60 seconds

--- src/app/business-domain/gallery/gallery-data.service.ts
+++ src/app/business-domain/gallery/gallery-data.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, map, Observable, shareReplay} from "rxjs";
+import {BehaviorSubject, map, Observable, shareReplay, switchMap, timer} from "rxjs";
 import {Document} from "../../data-domain/gallery/models/gallery";
 import {HttpClient} from "@angular/common/http";
 
@@ -12,117 +12,9 @@ export class GalleryDataService {
     constructor(private readonly httpClient: HttpClient) {
     }
 
-    public readonly documents$: Observable<Document[]> = this.httpClient.get<{documents: Document[]}>(apiUrl)
-        .pipe(
-            shareReplay(1, 120),
-            map(response => response.documents)
-        );
-
-    public readonly _documents$ = new BehaviorSubject<Document[]>([
-        {
-            id: 'test',
-            created: new Date(),
-            updated: null,
-            type: 'image',
-            path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-            previews: [
-                {
-                    width: 500,
-                    height: 500,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-                    mime: 'image/jpeg'
-                },
-                {
-                    width: 100,
-                    height: 100,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-320.jpg',
-                    mime: 'image/jpeg'
-                }
-            ]
-        },
-        {
-            id: 'test',
-            created: new Date(),
-            updated: null,
-            type: 'image',
-            path: 'https://source.unsplash.com/random/900×700/?wedding',
-            previews: [
-                {
-                    width: 500,
-                    height: 500,
-                    path: 'https://source.unsplash.com/random/500×500/?wedding',
-                    mime: 'image/jpeg'
-                },
-                {
-                    width: 100,
-                    height: 100,
-                    path: 'https://source.unsplash.com/random/100×100/?wedding',
-                    mime: 'image/jpeg'
-                }
-            ]
-        },
-        {
-            id: 'test',
-            created: new Date(),
-            updated: null,
-            type: 'image',
-            path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-            previews: [
-                {
-                    width: 500,
-                    height: 500,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-                    mime: 'image/jpeg'
-                },
-                {
-                    width: 100,
-                    height: 100,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-320.jpg',
-                    mime: 'image/jpeg'
-                }
-            ]
-        },
-        {
-            id: 'test',
-            created: new Date(),
-            updated: null,
-            type: 'image',
-            path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-            previews: [
-                {
-                    width: 500,
-                    height: 500,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-                    mime: 'image/jpeg'
-                },
-                {
-                    width: 100,
-                    height: 100,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-320.jpg',
-                    mime: 'image/jpeg'
-                }
-            ]
-        },
-        {
-            id: 'test',
-            created: new Date(),
-            updated: null,
-            type: 'image',
-            path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-            previews: [
-                {
-                    width: 500,
-                    height: 500,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-800w.jpg',
-                    mime: 'image/jpeg'
-                },
-                {
-                    width: 100,
-                    height: 100,
-                    path: 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/responsive-images/elva-320.jpg',
-                    mime: 'image/jpeg'
-                }
-            ]
-        }
-    ]);
+    public readonly documents$: Observable<Document[]> = timer(500, 60000).pipe(
+        switchMap(() => this.httpClient.get<{ documents: Document[] }>(apiUrl)),
+        shareReplay(1, 60),
+        map(response => response.documents)
+    );
 }
