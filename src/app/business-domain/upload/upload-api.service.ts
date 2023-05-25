43f4451b1535e5b3977c4be0136e8ca25e41498d
Pascal Heidmann <pascal@heidmann.io>
2023-05-25 09:15:36 +0200

    Fix upload, disable pwa

--- src/app/business-domain/upload/upload-api.service.ts
+++ src/app/business-domain/upload/upload-api.service.ts
@@ -1,9 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+    BehaviorSubject,
+    map,
+    Observable,
+    skipWhile,
+    Subject,
+    timer,
+    withLatestFrom
+} from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 import { UploadPreview } from '../../data-domain/upload/models/uploadPreview';
 import { Uuid } from '../../infrastructure/uid/uuid';
 import { GalleryDataService } from '../gallery/gallery-data.service';
+
+interface UploadQueueItem {
+    preview: UploadPreview;
+    request: Observable<any>;
+}
 
 @Injectable({
     providedIn: 'root',
@@ -15,7 +29,27 @@ export class UploadApiService {
     ) {
     }
 
-    public uploads: { preview: UploadPreview, request: Observable<any> }[] = [];
+    public uploads: UploadQueueItem[] = [];
+    public uploads$: Subject<UploadQueueItem[]> = new BehaviorSubject<UploadQueueItem[]>([]);
+
+    private uploadFinished$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+    private newUpload$: Subject<null> = new Subject();
+
+    private currentUpload$ = timer(1000, 1000).pipe(
+        withLatestFrom([this.newUpload$, this.uploadFinished$]),
+        distinctUntilChanged(),
+        map((): UploadQueueItem | undefined => this.uploads.length > 0 ? this.uploads.shift() : undefined),
+        // @ts-ignore
+        skipWhile((item: UploadQueueItem | undefined): boolean => item === undefined),
+        map((item: UploadQueueItem) => item.request.subscribe({
+            complete: () => {
+                this.uploads$.next(this.uploads);
+                this.galleryDataService.refresh();
+                this.uploadFinished$.next(1);
+            }
+        }))
+    ).subscribe(() => {
+    });
 
     public upload(file: File, albumId: Uuid): void {//Observable<FileUpload> {
         let formData = new FormData();
@@ -25,19 +59,17 @@ export class UploadApiService {
             `/api/album/${albumId}/upload`,
             formData
         );
-        request.subscribe({
-            complete: () => {
-                this.galleryDataService.refresh();
-            }
-        });
-        this.uploads.push({
+        const nextUpload = {
             preview: {
                 file: file,
                 previewImage: URL.createObjectURL(file),
                 isImage: file.type.startsWith('image'),
                 isVideo: file.type.startsWith('video'),
             },
-            request: request
-        });
+            request: request,
+        };
+
+        this.uploads.push(nextUpload);
+        this.uploads$.next(this.uploads);
     }
 }
